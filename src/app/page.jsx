163ee3b358458d72d7de0099ea0f44426efa74c9
Bartosz Art <bartosz.art@gmail.com>
2023-03-06 20:47:34 +0100

    Rearranged metadata

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,6 +2,10 @@ import { Inter } from 'next/font/google';
 
 const inter = Inter({ subsets: ['latin'] });
 
+export const metadata = {
+	title: 'Focus Beauty | strona domowa',
+};
+
 export default function Home() {
 	return <main></main>;
 }
